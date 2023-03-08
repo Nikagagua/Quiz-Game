2b1be22efdabbb4e5ada30fbd2375d4a23e0d70d
Nika Gagua <97543752+Nikagagua@users.noreply.github.com>
2023-03-08 11:09:36 +0000

    Initial commit

---
+++ data.py
@@ -0,0 +1,226 @@
+question_data = [{
+  "category": "Entertainment: Board Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Snakes and Ladders was originally created in India?",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "The Mann Co. Store from Team Fortress 2 has the slogan   We hire mercenaries and get in fights  .",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "General Knowledge",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Gumbo is a stew that originated in Louisiana.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Music",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Eurobeat is primarily an Italian genre of music.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Science: Computers",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Linus Torvalds created Linux and Git.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Music",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "American rapper Dr. Dre actually has a Ph.D. doctorate.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Film",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "The 2010 film   The Social Network   is a biographical drama film about MySpace founder Tom Anderson.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "Faust is a playable character in   Guilty Gear Xrd Revelator  .",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Science: Mathematics",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "The &#039;Squaring the Circle&#039; problem is solvable.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Sports",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Peyton Manning retired after winning Super Bowl XLIX.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Film",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "In the original script of   The Matrix  , the machines used humans as additional computing power instead of batteries.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Japanese Anime & Manga",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "In Kill La Kill, the weapon of the main protagonist is a katana. ",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "In   Sonic Adventure  , you are able to transform into Super Sonic at will after completing the main story.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "General Knowledge",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "You can legally drink alcohol while driving in Mississippi.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Board Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "The card game   Munchkin   won the 2001 Origins Award for Best Traditional Card Game.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "History",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "The ultimate phrase used by Pharah from Overwatch is:   Justice rains from above!  ",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Animals",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Rabbits are rodents.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Books",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "Stephen Chbosky wrote the book &#039;The Perks of Being A Wallflower&#039;.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "Rebecca Chambers does not appear in any Resident Evil except for the original Resident Evil and the Gamecube remake.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Science: Computers",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "  HTML   stands for Hypertext Markup Language.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Science: Computers",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Time on Computers is measured via the EPOX System.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Science & Nature",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "An exothermic reaction is a chemical reaction that releases energy by radiating electricity.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Music",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Daft Punk originated in France.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Music",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "Stevie Wonder&#039;s real name is Stevland Hardaway Morris.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Science: Mathematics",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "The sum of any two odd integers is odd.",
+  "correct_answer": "False",
+  "incorrect_answers": ["True"]
+}, {
+  "category": "Entertainment: Video Games",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "  Sonic the Hedgehog 2   originally was going to have a time travel system.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Entertainment: Books",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question": "  Green Eggs and Ham   consists of only 50 different words.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}, {
+  "category": "Vehicles",
+  "type": "boolean",
+  "difficulty": "easy",
+  "question":
+  "BMW M GmbH is a subsidiary of BMW AG that focuses on car performance.",
+  "correct_answer": "True",
+  "incorrect_answers": ["False"]
+}]
